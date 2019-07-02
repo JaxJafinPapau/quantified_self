f91e7fd7233864475b281ae3f7e9824f5ccd4496
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-02 13:25:46 -0600

    Removes comments

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -9,14 +9,10 @@ router.post('/', async function(req, res, next) {
       name: req.body.food.name,
       calories: req.body.food.calories
     })
-    // I'd like to refactor this setHeader out
     res.setHeader(...defaultHeader)
     res.status(201).send(JSON.stringify(food))
   } catch (error) {
     res.setHeader(...defaultHeader)
-    // This error isn't throwing. Instead, the server accepts null values
-    // and the only errors that get thrown are sequelize errors that fail
-    // validations, such as passing a string for the calories
     res.status(400).send({ error: "Invalid Parameters" })
   }
 })
