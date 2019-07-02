74015636f35885aab960e741e54a4fde6976ab66
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-07-02 13:38:09 -0600

    Adds allowNull: false validation to all Food datatypes

--- models/food.js
+++ models/food.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Food = sequelize.define('Food', {
-    name: DataTypes.STRING,
-    calories: DataTypes.INTEGER
+    name: { type: DataTypes.STRING, allowNull: false },
+    calories: { type: DataTypes.INTEGER, allowNull: false }
   }, {});
   Food.associate = function(models) {
     Food.belongsToMany(models.Meal, { through: 'MealFood' })
