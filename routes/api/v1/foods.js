04a85a0a3778e2f30118f5338d051d2503d57173
wipegup <wipegup@gmail.com>
2019-07-02 07:28:39 -0600

    Add get all foods route

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Food = require('../../../models').Food
+var defaultHeader = ["Content-Type", "application/json"];
 
 router.post('/', async function(req, res, next) {
   try {
@@ -9,14 +10,25 @@ router.post('/', async function(req, res, next) {
       calories: req.body.food.calories
     })
     // I'd like to refactor this setHeader out
-    res.setHeader("Content-Type", "application/json")
+    res.setHeader(...defaultHeader)
     res.status(201).send(JSON.stringify(food))
   } catch (error) {
-    res.setHeader("Content-Type", "application/json")
+    res.setHeader(...defaultHeader)
     // This error isn't throwing. Instead, the server accepts null values
     // and the only errors that get thrown are sequelize errors that fail
     // validations, such as passing a string for the calories
     res.status(400).send({ error: "Invalid Parameters" })
+  }
+})
+
+router.get('/', async function(req, res, next){
+  try {
+    let foods = await Food.findAll();
+    res.setHeader(...defaultHeader);
+    res.status(200).send(JSON.stringify(foods));
+  } catch (error) {
+    res.setHeader(...defaultHeader);
+    res.status(400).send(error);
   }
 })
 
