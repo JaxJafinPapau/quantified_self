a3b3d8a0657a3e75b861644249178fb747a12275
wipegup <wipegup@gmail.com>
2019-07-02 15:09:40 -0600

    Add relation addition for meals and food

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Meal = require('../../../models').Meal;
 var Food = require('../../../models').Food;
+var MealFood = require('../../../models').MealFood;
 var defaultHeader = ["Content-Type", "application/json"];
 
 router.post('/:meal_id/foods/:id', async function(req, res, next){
@@ -10,6 +11,7 @@ router.post('/:meal_id/foods/:id', async function(req, res, next){
     let meal = await Meal.findByPk(req.params.meal_id);
     let food = await Food.findByPk(req.params.id);
     if (meal != null && food != null){
+      await MealFood.create({FoodId: food.id, MealId: meal.id})
       res.setHeader(...defaultHeader);
       let message = `Successfully added ${food.name} to ${meal.name}`
       console.log(message);
